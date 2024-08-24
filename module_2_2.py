afa096c91d84ad44c719ad8dc1b00ef045caa05e
Alex <144250961+Arhosel@users.noreply.github.com>
2024-08-25 01:02:59 +0500

    Update module_2_2.py

--- module_2_2.py
+++ module_2_2.py
@@ -3,7 +3,11 @@ second = input('Введите второе число: ')
 third = input('Введите третье число: ')
 if first == second == third:
     print(3)
-elif first == second or second == third or first == third:          #== Равенство в Pyton
+elif (first == second
+      or second == third
+      or first == third):          #== Равенство в Pyton
     print(2)
-elif first != second and second != third and first != third:        #!= Неравенство в Pyton
+elif (first != second
+      and second != third
+      and first != third):        #!= Неравенство в Pyton
     print(0)
