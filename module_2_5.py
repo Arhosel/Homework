5f9dabc30ddd54e1efeb19680ec2db5ddfc3225d
Alex <144250961+Arhosel@users.noreply.github.com>
2024-08-28 18:56:02 +0500

    Update module_2_5.py

--- module_2_5.py
+++ module_2_5.py
@@ -5,7 +5,7 @@ def get_matrix(n, m, vaule):
         for g in range(m):                          #создаём внешний цикл for для количества столбцов матрицы с m повторений
            row.append(vaule)
         matrix.append(row)
-    return matrix                                   #возврат значения переменной matrix после выполнения всех циклов
+    return matrix                                   #возврат значения списка matrix после выполнения всех циклов
 
 result1 = get_matrix(2, 2, 10)                      #исходный код задачи со значениями переменных
 result2 = get_matrix(3, 5, 42)
