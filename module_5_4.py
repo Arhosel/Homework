de8aa26b21986e5fc012b87f295d76dd8e63db89
Alex <144250961+Arhosel@users.noreply.github.com>
2024-10-02 21:02:48 +0500

    Create module_5_4.py

---
+++ module_5_4.py
@@ -0,0 +1,75 @@
+class House:
+    houses_history = []
+    def __new__(cls, *args, **kwargs):
+        cls.houses_history.append(args[0])
+        return object.__new__(cls)
+
+    def __init__(self, name, number_of_floors):
+        self.name = name
+        self.number_of_floors = number_of_floors
+
+    def __del__(self):
+        print(f'{self.name} снесен, но он останется в истории')
+
+    def go_to(self, new_floor):
+        self.new_floor = new_floor
+        if self.new_floor < 1 or self.new_floor > self.number_of_floors:
+            print("Такого этажа не существует")
+        else:
+            for self.new_floor in range(1, new_floor + 1):
+                print(self.new_floor)
+
+        def __str__(self):
+            return f'Название ЖК: {self.name}, количество этажей: {self.number_of_floors}'
+
+        # Метод, возвращащий F строку с нужными параметрами
+        def __len__(self):
+            return self.number_of_floors
+
+        # Метод, возвращающий количество этажей здания
+        def __eq__(self, other):
+            return self.number_of_floors == other.number_of_floors
+
+        def __add__(self, value):
+            if isinstance(value, int):
+                self.number_of_floors += value
+                return self
+
+        def __radd__(self, value):
+            self.number_of_floors += value
+            return self
+
+        def __iadd__(self, value):
+            if isinstance(value, int):
+                self.number_of_floors += value
+                return self
+
+        def __gt__(self, other):
+            return self.number_of_floors > other.number_of_floors
+
+        def __ge__(self, other):
+            return self.number_of_floors >= other.number_of_floors
+
+        def __lt__(self, other):
+            return self.number_of_floors < other.number_of_floors
+
+        def __le__(self, other):
+            return self.number_of_floors <= other.number_of_floors
+
+        def __ne__(self, other):
+            return self.number_of_floors != other.number_of_floors
+    # Различные методы, по которым будет проводиться проверка
+
+#Пример выполняемого кода из задания
+h1 = House('ЖК Эльбрус', 10)
+print(House.houses_history)
+h2 = House('ЖК Акация', 20)
+print(House.houses_history)
+h3 = House('ЖК Матрёшки', 20)
+print(House.houses_history)
+
+# Удаление объектов
+del h2
+del h3
+
+print(House.houses_history)
