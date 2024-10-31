dc94e111798e6563907a0c49167c023e3c42b3ec
Alex <144250961+Arhosel@users.noreply.github.com>
2024-10-31 23:49:55 +0500

    Create module_9_4.py

---
+++ module_9_4.py
@@ -0,0 +1,32 @@
+from random import choice
+
+#Lambda-функция:
+first = 'Мама мыла раму'
+second = 'Рамена мало было'
+res = list(map(lambda x, y: x == y, first, second))
+print(res)
+
+#Замыкание:
+def get_advanced_writer(file_name):
+    def write_everything(*data_set):                                                                                    #data_set - параметр принимающий неограниченное
+        with open(file_name, mode='w', encoding='utf-8') as file:                                                       #количество данных любого типа
+            for i in data_set:
+                file.write(f"{i}\n")
+    return write_everything
+
+write = get_advanced_writer('example.txt')
+write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
+
+#Метод __call__:
+class MysticBall:
+    def __init__(self, *words: str):                                                                                    #класс MysticBall, объекты которого обладают
+        self.words = words                                                                                              #атрибутом words хранящий коллекцию строк
+
+    def __call__(self):                                                                                                 #метод __call__ который будет случайным образом
+        return choice(self.words)                                                                                       #выбирать слово из words и возвращать его
+
+#Исходный код из задания
+first_ball = MysticBall('Да', 'Нет', 'Наверное')
+print(first_ball())
+print(first_ball())
+print(first_ball())
