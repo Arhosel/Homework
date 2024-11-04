3800568c7e11732b644ba2d3937f797ff40f230e
Alex <144250961+Arhosel@users.noreply.github.com>
2024-11-04 13:31:12 +0500

    Create module_9_6.py

---
+++ module_9_6.py
@@ -0,0 +1,12 @@
+def all_variants(text):
+    x = 0                                                                                                               #функция-генератор 
+    y = 0                                                                                                               #принимает строку text и возвращает объект-генератор
+    for x in range(len(text)):
+        for y in range(x+1, len(text)+1):
+            yield text[x:y]
+
+
+#Пример выполняемого кода из задания
+a = all_variants("abc")
+for i in a:
+    print(i)
