dda8edc02184cf0297dd7bdd81bb194c874f8dad
Alex <144250961+Arhosel@users.noreply.github.com>
2024-11-15 09:11:53 +0500

    Update module_10_3.py

--- module_10_3.py
+++ module_10_3.py
@@ -15,10 +15,10 @@ class Bank:
         for i in range(100):                                                                                            #метод совершает 100 транзакций пополнения средств
             if self.balance >= 500 and self.lock.locked():                                                              #разблокировка методом release при балансе > 500
                 self.lock.release()
-            random_int = random.randint(50, 500)                                                                  #случайное пополнение на число от 50 до 500
+            random_int = random.randint(50, 500)                                                                        #случайное пополнение на число от 50 до 500
             self.balance += random_int
             print(f'Пополнение: {random_int}. Баланс: {self.balance}')                                                  #вывод строки результата пополнения
-            time.sleep(0.1)                                                                                           #ожидание в 0.001 секунды-имитация скорости выполнения
+            time.sleep(0.1)                                                                                             #ожидание в 0.001 секунды-имитация скорости выполнения
 
 
     def take(self):
